d3dbb8ea9203bcbbea3bca5e65463a1dcb82a996
Garrett van Wageningen <ghvanw283@gmail.com>
2020-01-10 18:44:02 -0600

    initish commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import katex from "katex";
+
+const math = document.querySelector("#math");
+
+const Katex = (tex: string) => {
+    const span = document.createElement("span");
+
+    katex.render(tex, span, {
+        output: "html",
+        throwOnError: true
+    });
+
+    return span;
+}
+
+math?.appendChild(Katex("\\forall x (P(x) \\to Q(x))"));
